5d436c905b85388855f8d20b6a4dbe010017a77f
adtyainash <16521107@mahasiswa.itb.ac.id>
2024-05-16 09:34:27 +0700

    new

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // TODO: Sesuaikan dengan base url front-end yang digunakan [DONE]
-$config['base_url'] = 'http://34.101.117.72';
+$config['base_url'] = 'http://34.101.101.188';
 
 /*
 |--------------------------------------------------------------------------
